c87f86196a743fff528fe4726f747af2f41c5c01
Prince Jackson <39716145+pjack484@users.noreply.github.com>
2019-09-23 22:31:20 -0400

    Add files via upload

---
+++ mileage.py
@@ -0,0 +1,7 @@
+print("How many kilometers did you cycle today?")
+kms = input()
+miles = float(kms)/1.60934
+print(f"That is equal to {round(miles, 2)} miles ")
+
+# round(thing to round, how many decimal points)
+
